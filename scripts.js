4fd2a11924fc8c9ad8c40b41296e45cadeca87da
timplay33 <tim3play3@gmail.com>
2023-01-09 21:02:56 +0100

    modified:   index.html modified:   scripts.js 	modified:   style.css

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
-console.log("hello world!")
-document.getElementById("")
+/// intro start
+let intro = document.getElementById("intro")
+intro.innerHTML = "<span class='introText'>Welcome</span>";
+
+setTimeout(function(){
+    intro.innerHTML = "";
+}, 3000);
+
+/// intro end
+
